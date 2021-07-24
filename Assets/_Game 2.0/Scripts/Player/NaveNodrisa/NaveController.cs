d4b24ce25b033a13d4868e65107ef2826a5e5d18
DESKTOP-TS22UMM\RAUL <akeymi0.0@gmail.com>
2021-07-23 19:56:10 -0500

    NavMesh

--- Assets/_Game 2.0/Scripts/Player/NaveNodrisa/NaveController.cs
+++ Assets/_Game 2.0/Scripts/Player/NaveNodrisa/NaveController.cs
@@ -1,20 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NaveController : MonoBehaviour, IDamagable
 {
     [SerializeField] int life = 10;
     [SerializeField] int shieldLife = 10;
     [SerializeField] GameObject shield = default;
+    [SerializeField] NavMeshAgent agent = default;
 
     private int currentLife;
+    private int currentShieldLife;
+    private CharacterController player;
 
     private void Start()
     {
         currentLife = life;
+        currentShieldLife = shieldLife;
+        player = FindObjectOfType<CharacterController>();
     }
 
+    private void Update()
+    {
+        MoveNave();
+    }
+
+    private void MoveNave()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+
+    public void ShieldDamage(int amount)
+    {
+        currentShieldLife -= amount;
+
+        if(currentShieldLife <= 0)
+        {
+            shield.SetActive(false);
+        }
+    }
 
     public void Damage(int amount)
     {
@@ -24,5 +49,15 @@ public class NaveController : MonoBehaviour, IDamagable
         {
             //Dead
         }
+    }
+
+    public void HealNave()
+    {
+
+    }
+
+    public void RepairShield()
+    {
+
     }
 }
--- Assets/_Game 2.0/Scripts/Room/RoomTrigger.cs
+++ Assets/_Game 2.0/Scripts/Room/RoomTrigger.cs
@@ -8,7 +8,7 @@ public class RoomTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Nave"))
         {
             room.ActivateRoom();
         }
